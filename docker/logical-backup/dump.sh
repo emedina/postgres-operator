94e61ff8e13c698bd70d1f7661186aa49b7b536f
Enrique Medina Montenegro <emedinamontenegro@epo.org>
2020-04-08 10:17:06 +0200

    EPO

--- docker/logical-backup/dump.sh
+++ docker/logical-backup/dump.sh
@@ -6,77 +6,20 @@ set -o nounset
 set -o pipefail
 IFS=$'\n\t'
 
-ALL_DB_SIZE_QUERY="select sum(pg_database_size(datname)::numeric) from pg_database;"
-PG_BIN=$PG_DIR/$PG_VERSION/bin
-DUMP_SIZE_COEFF=5
 ERRORCOUNT=0
-
-TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-K8S_API_URL=https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1
-CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-
-function estimate_size {
-    "$PG_BIN"/psql -tqAc "${ALL_DB_SIZE_QUERY}"
-}
 
 function dump {
     # settings are taken from the environment
-    "$PG_BIN"/pg_dumpall
+    "$PGPASSWORD" "$PG_BIN"/pg_dump -U "$PGUSER" --no-owner -h "$PGHOST"
 }
 
 function compress {
     pigz --best
 }
 
-function get_pods {
-    declare -r SELECTOR="$1"
-
-    curl "${K8S_API_URL}/namespaces/${POD_NAMESPACE}/pods?$SELECTOR" \
-        --cacert $CERT \
-        -H "Authorization: Bearer ${TOKEN}" | jq .items[].status.podIP -r
-}
-
-function get_current_pod {
-    curl "${K8S_API_URL}/namespaces/${POD_NAMESPACE}/pods?fieldSelector=metadata.name%3D${HOSTNAME}" \
-        --cacert $CERT \
-        -H "Authorization: Bearer ${TOKEN}"
-}
-
-declare -a search_strategy=(
-    list_all_replica_pods_current_node
-    list_all_replica_pods_any_node
-    get_master_pod
-)
-
-function list_all_replica_pods_current_node {
-    get_pods "labelSelector=${CLUSTER_NAME_LABEL}%3D${SCOPE},spilo-role%3Dreplica&fieldSelector=spec.nodeName%3D${CURRENT_NODENAME}" | head -n 1
-}
-
-function list_all_replica_pods_any_node {
-    get_pods "labelSelector=${CLUSTER_NAME_LABEL}%3D${SCOPE},spilo-role%3Dreplica" | head -n 1
-}
-
-function get_master_pod {
-    get_pods "labelSelector=${CLUSTER_NAME_LABEL}%3D${SCOPE},spilo-role%3Dmaster" | head -n 1
-}
-
-CURRENT_NODENAME=$(get_current_pod | jq .items[].spec.nodeName --raw-output)
-export CURRENT_NODENAME
-
-for search in "${search_strategy[@]}"; do
-
-    PGHOST=$(eval "$search")
-    export PGHOST
-
-    if [ -n "$PGHOST" ]; then
-        break
-    fi
-
-done
-
 set -x
 # use $LOGICAL_BACKUP_S3_BUCKET to refer to the network folder to leave this backup.
-PATH_TO_BACKUP=$LOGICAL_BACKUP_S3_BUCKET"/logical_backups/"$(date +%s).sql.gz
+PATH_TO_BACKUP=$PATH_TO_BACKUP/$(date +%s).sql.gz
 
 dump | compress >"$PATH_TO_BACKUP"
 [[ ${PIPESTATUS[0]} != 0 || ${PIPESTATUS[1]} != 0 || ${PIPESTATUS[2]} != 0 ]] && (( ERRORCOUNT += 1 ))
